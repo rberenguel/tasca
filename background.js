25bfbd7067d4d1b447cf372788d74a439727ffb1
Ruben Berenguel Montoro <ruben@mostlymaths.net>
2026-01-03 08:52:58 +0100

    Minihack

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({ url: chrome.runtime.getURL('index.html') })
+chrome.action.onClicked.addListener(async () => {
+  const url = chrome.runtime.getURL('index.html')
+  const tabs = await chrome.tabs.query({ url })
+
+  if (tabs.length > 0) {
+    // Focus existing tab
+    await chrome.tabs.update(tabs[0].id, { active: true })
+    await chrome.windows.update(tabs[0].windowId, { focused: true })
+  } else {
+    chrome.tabs.create({ url })
+  }
 })
